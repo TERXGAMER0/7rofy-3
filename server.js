655b0e7a7f46a5fc5968c7890f0211d8561d47d0
TERXGAMER0 <TerXGamer123@gmail.com>
2025-04-16 03:14:30 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,14 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
-app.use(express.static(__dirname));
+
+// تقديم صفحة التفعيل (passystem.html) عبر الراوت الجذر، بحيث لا تكون ضمن المحتويات الثابتة
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "passystem.html"));
+});
+
+// تقديم الملفات الثابتة من مجلد "client" لصفحة اللعبة وكل الملفات الخاصة بها
+app.use(express.static(path.join(__dirname, "client")));
 
 const validCodes = process.env.VALID_CODES ? process.env.VALID_CODES.split(",") : [];
 
